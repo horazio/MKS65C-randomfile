5f19f2fcc0db00752c8b6e62a72db6076552972a
horazio <hfusco@stuy.edu>
2018-10-29 17:40:03 -0400

    reads from, writes into, then reads back from a new file

--- rand.c
+++ rand.c
@@ -11,15 +11,12 @@ int * rando_gen(int size){
   return nums;
 }
 
-void rando_write(int * nums, int size){
-  char path[20];
-  strcpy(path, "./");
-  strcpy(path, (char) nums[0]);
-  strcpy(path, ".txt");
-  printf("%s", path);
-  open(path,  O_WRONLY);
-  //write()
-
+char * rando_write(int * nums, int size){
+  char * path = malloc(20);
+  sprintf(path, "./%d.txt", nums[0]);
+  int fd = open(path,  O_WRONLY | O_CREAT, 0770);
+  write(fd, nums, size);
+  return path;
 }
 
 int main(){
@@ -31,7 +28,22 @@ int main(){
     printf("nums[%d] = %d\n",i, nums[i]);
   }
 
+  char * path =rando_write(nums, size * 4);
+
+  int * nums2 = malloc(4 * size);
+  int fd = open(path, O_RDONLY);
+  
+  read(fd, nums2, size * 4);
+
+  printf("The next array copied back from the file is: \n");
+  
+  for(i = 0; i < size; i++){
+    printf("nums2[%d] = %d\n",i, nums2[i]);
+  }
+
+  
   printf("\n\n");
   free(nums);
+  free(nums2);
   return 0;
 }
